3282b05aa55322ca5b60b6c35e2ea1dadb3fd8c2
Gleb Gutnik <glebgytnik@gmail.com>
2025-12-14 21:08:52 +0300

    Optimize animations

--- shared/src/commonMain/kotlin/io/github/themeanimator/ThemeAnimationModifier.kt
+++ shared/src/commonMain/kotlin/io/github/themeanimator/ThemeAnimationModifier.kt
@@ -1,7 +1,11 @@
 package io.github.themeanimator
 
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.tween
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.graphics.layer.GraphicsLayer
 import androidx.compose.ui.node.DrawModifierNode
 import androidx.compose.ui.node.ModifierNodeElement
 import androidx.compose.ui.node.invalidateDraw
@@ -9,61 +13,89 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 internal fun <T> Modifier.themeAnimation(
-    state: ThemeAnimationState<T>,
-) = this then ThemeAnimationElement(state)
+
+    theme: T,
+    graphicsLayer: GraphicsLayer,
+) = this then ThemeAnimationElement(theme, graphicsLayer)
 
 internal data class ThemeAnimationElement<T>(
-    val state: ThemeAnimationState<T>,
+    val theme: T,
+    val graphicsLayer: GraphicsLayer,
 ) : ModifierNodeElement<ThemeAnimationNode<T>>() {
     override fun create(): ThemeAnimationNode<T> {
-        return ThemeAnimationNode(state)
+        return ThemeAnimationNode(theme, graphicsLayer)
     }
 
     override fun update(node: ThemeAnimationNode<T>) {
-        node.updateState(state)
+        node.updateState(theme, graphicsLayer)
     }
 }
 
 internal class ThemeAnimationNode<T>(
-    private var state: ThemeAnimationState<T>,
+    private var theme: T,
+    private var graphicsLayer: GraphicsLayer,
 ) : Modifier.Node(), DrawModifierNode {
+
+    private var isAnimating = false
+    private var animationProgress = 0f
+    private var prevImageBitmap: ImageBitmap? = null
+    private var currentImageBitmap: ImageBitmap? = null
+    private var prevTheme: T? = null
+
+    private var animationObserverJob: Job? = null
 
     override fun onAttach() {
         super.onAttach()
-        observeTheme()
+        coroutineScope.launch {
+            val image = graphicsLayer.toImageBitmap()
+            prevImageBitmap = image
+            currentImageBitmap = image
+        }
     }
 
     fun updateState(
-        newState: ThemeAnimationState<T>,
+        newTheme: T,
+        newGraphicsLayer: GraphicsLayer,
     ) {
-        state = newState
-        observeTheme()
+        graphicsLayer = newGraphicsLayer
+        prevTheme = theme
+        theme = newTheme
+        if (prevTheme != theme) {
+            runAnimation()
+        }
     }
 
-    private var animationObserverJob: Job? = null
-    private fun observeTheme() {
+    private fun runAnimation() {
+        isAnimating = true
         animationObserverJob?.cancel()
         animationObserverJob = coroutineScope.launch {
-            state.animationProgress.collect {
+            prevImageBitmap = currentImageBitmap
+            currentImageBitmap = graphicsLayer.toImageBitmap()
+            animate(
+                initialValue = 0f,
+                targetValue = 1f,
+                animationSpec = tween(3000)
+            ) { value, _ ->
+                animationProgress = value
                 invalidateDraw()
             }
+            isAnimating = false
         }
     }
 
     override fun ContentDrawScope.draw() {
-        val old = state.prevImageBitmap
-        val new = state.currentImageBitmap
-        val alpha = state.animationProgress.value
-        val isAnimating = state.isAnimating
+        val old = prevImageBitmap
+        val new = currentImageBitmap
+        val alpha = animationProgress
+        val isAnim = isAnimating
 
-        if (old != null && isAnimating) {
+        if (old != null && new != null && isAnim) {
             drawImage(old, alpha = 1f)
-        }
-        if (new != null && isAnimating) {
             drawImage(new, alpha = alpha)
-        }
-
-        if (!isAnimating || old == null) {
+        } else {
+            graphicsLayer.record {
+                this@draw.drawContent()
+            }
             drawContent()
         }
     }
--- shared/src/commonMain/kotlin/io/github/themeanimator/ThemeAnimationScope.kt
+++ shared/src/commonMain/kotlin/io/github/themeanimator/ThemeAnimationScope.kt
@@ -2,10 +2,7 @@ package io.github.themeanimator
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.graphics.layer.drawLayer
 import androidx.compose.ui.graphics.rememberGraphicsLayer
 
 @Composable
@@ -14,22 +11,9 @@ fun <T> ThemeAnimationScope(
     content: @Composable () -> Unit,
 ) {
     val graphicsLayer = rememberGraphicsLayer()
-    val animationState = rememberThemeAnimationState(
-        theme = theme,
-        graphicsLayer = graphicsLayer
-    )
-
-    LaunchedEffect(theme) {
-        animationState.updateCurrentTheme(theme)
-    }
 
     Box(
-        modifier = Modifier.drawWithContent {
-            graphicsLayer.record {
-                this@drawWithContent.drawContent()
-            }
-            drawLayer(graphicsLayer)
-        }.themeAnimation(animationState)
+        modifier = Modifier.themeAnimation(theme, graphicsLayer)
     ) {
         content()
     }

d034c79764eca36aeb8cde0a49b5c6213a6dc42f
Gleb Gutnik <glebgytnik@gmail.com>
2025-12-14 18:28:59 +0300

    Move from deprecated APIs

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -51,10 +51,10 @@ kotlin {
             implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(projects.shared)
+            implementation(libs.kmp.tooling.preview)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)

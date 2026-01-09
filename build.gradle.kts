eed70c4a3067b086e4bfb9057bb39094191e4a33
Gleb Gutnik <glebgytnik@gmail.com>
2026-01-09 07:29:00 +0300

    Add Dokka generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.dokka.gradle.DokkaTaskPartial
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -7,4 +9,23 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.dokka)
+}
+
+dokka {
+    dokkaPublications {
+        html {
+            outputDirectory.set(layout.buildDirectory.dir("dokka/html"))
+        }
+    }
+}
+
+dependencies {
+    dokka(projects.shared)
+    dokka(projects.storage)
+}
+
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
 }

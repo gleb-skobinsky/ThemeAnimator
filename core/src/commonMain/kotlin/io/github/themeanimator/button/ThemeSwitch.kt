5ddf8ec1910433ed56518b334f3ca5eda96c5c5d
gleb-skobinsky <glebgytnik@gmail.com>
2026-03-16 23:31:25 +0300

    Add a more customizable theme switch

--- core/src/commonMain/kotlin/io/github/themeanimator/button/ThemeSwitch.kt
+++ core/src/commonMain/kotlin/io/github/themeanimator/button/ThemeSwitch.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
